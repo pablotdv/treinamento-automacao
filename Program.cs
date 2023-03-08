0b636510a640c5294bbfce12eb1c0792cf7924ec
Pablo T. de Vargas <pablo.vargas@cwi.com.br>
2023-03-08 17:09:52 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+new DriverManager().SetUpDriver(new EdgeConfig());
+var driver = new EdgeDriver();
+
+Thread.Sleep(5000); 
+
+driver.Navigate().GoToUrl("https://www.google.com/");
+
+var textInput = driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+textInput.SendKeys("Selenium");
+//textInput.SendKeys(Keys.Enter);
+
+Thread.Sleep(5000);
+
+var submitInput = driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[5]/center/input[1]"));
+submitInput.Click();
+
+
+Thread.Sleep(5000); 
+driver.Quit();
